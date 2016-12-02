d53e365c81155ff1b10525971bf2ab9d9a489d27
angeloorrico <a.orrico@gmail.com>
2016-12-01 22:09:04 -0300

    Incrementing the database version and updating the Readme screenshots.

--- app/src/main/java/br/com/angeloorrico/popularmovies/database/MovieDatabaseHelper.java
+++ app/src/main/java/br/com/angeloorrico/popularmovies/database/MovieDatabaseHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MovieDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME  = "PopularMovies.db";
-    public static final int DATABASE_VERSION  = 1;
+    public static final int DATABASE_VERSION  = 2;
 
     public MovieDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
